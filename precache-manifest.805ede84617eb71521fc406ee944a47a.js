06b91b9e7c34e8c95275d25f60ff308044c489de
ghaidan <you@example.com>
2019-01-23 17:10:42 -0800

    Updates

---
+++ precache-manifest.805ede84617eb71521fc406ee944a47a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c0170acadf8ccb329a1",
+    "url": "/ghaidan92.github.io/memory-game-ReactJS/static/css/main.e2d7ecc0.chunk.css"
+  },
+  {
+    "revision": "6c0170acadf8ccb329a1",
+    "url": "/ghaidan92.github.io/memory-game-ReactJS/static/js/main.6c0170ac.chunk.js"
+  },
+  {
+    "revision": "0960eb802e0fe08cb90e",
+    "url": "/ghaidan92.github.io/memory-game-ReactJS/static/js/1.0960eb80.chunk.js"
+  },
+  {
+    "revision": "a8a1d45924281fe1dba3",
+    "url": "/ghaidan92.github.io/memory-game-ReactJS/static/js/runtime~main.a8a1d459.js"
+  },
+  {
+    "revision": "5a76d1dbc5b5cf4302e068d53590eddc",
+    "url": "/ghaidan92.github.io/memory-game-ReactJS/index.html"
+  }
+];
